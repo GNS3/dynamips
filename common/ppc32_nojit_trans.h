177ed79408db69464895fe7966d15d697b1ad393
Flávio J. Saraiva <flaviojs2005@gmail.com>
2014-05-19 06:33:42 +0100

    Fix unused variable warning in ppc32_jit.c.

--- common/ppc32_nojit_trans.h
+++ common/ppc32_nojit_trans.h
@@ -15,8 +15,8 @@
 #define JIT_SUPPORT 0
 
 /* Wrappers to x86-codegen functions */
-#define ppc32_jit_tcb_set_patch(a,b)
-#define ppc32_jit_tcb_set_jump(a,b)
+#define ppc32_jit_tcb_set_patch(a,b) (void)(a); (void)(b)
+#define ppc32_jit_tcb_set_jump(a,b)  (void)(a); (void)(b)
 
 /* PPC instruction array */
 extern struct ppc32_insn_tag ppc32_insn_tags[];
