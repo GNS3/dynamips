58b0dbe860fb6fc6025a805636438dcaeba6e7ca
grossmj <grossmj@gns3.net>
2019-07-12 11:41:16 +0200

    Dynamips 0.2.21

--- common/dynamips.c
+++ common/dynamips.c
@@ -73,7 +73,7 @@ const char *os_name = STRINGIFY(OSNAME);
 const char *sw_version = DYNAMIPS_VERSION"-"JIT_ARCH;
 
 /* Software version tag */
-const char *sw_version_tag = "2019022012";
+const char *sw_version_tag = "2019071212";
 
 /* Hypervisor */
 int hypervisor_mode = 0;
