1ca9aefedc19f5a1eee3ce6562fc6cdd148886ad
Flávio J. Saraiva <flaviojs2005@gmail.com>
2024-03-11 09:54:37 +0000

    Fix resource leaks in ppc32_load_elf_image

--- unstable/ppc32.c
+++ unstable/ppc32.c
@@ -561,17 +561,21 @@ int ppc32_load_elf_image(cpu_ppc_t *cpu,char *filename,int skip_load,
 
    if (elf_version(EV_CURRENT) == EV_NONE) {
       fprintf(stderr,"load_elf_image: library out of date\n");
+      close(fd);
       return(-1);
    }
 
    if (!(img_elf = elf_begin(fd,ELF_C_READ,NULL))) {
       fprintf(stderr,"load_elf_image: elf_begin: %s\n",
               elf_errmsg(elf_errno()));
+      close(fd);
       return(-1);
    }
 
    if (!(ehdr = elf32_getehdr(img_elf))) {
       fprintf(stderr,"load_elf_image: invalid ELF file\n");
+      elf_end(img_elf);
+      close(fd);
       return(-1);
    }
 
@@ -580,6 +584,8 @@ int ppc32_load_elf_image(cpu_ppc_t *cpu,char *filename,int skip_load,
 
    if (!bfd) {
       perror("load_elf_image: fdopen");
+      elf_end(img_elf);
+      close(fd);
       return(-1);
    }
 
@@ -609,6 +615,8 @@ int ppc32_load_elf_image(cpu_ppc_t *cpu,char *filename,int skip_load,
             if (!haddr) {
                fprintf(stderr,"load_elf_image: invalid load address 0x%x\n",
                        vaddr);
+               elf_end(img_elf);
+               fclose(bfd);
                return(-1);
             }
 
